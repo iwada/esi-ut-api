0ccb0585121766a38fbc7d8654a12cfdad6f3f5c
Iwada Eja <Iwada@twalle.com>
2022-04-25 22:35:53 +0300

    MR6: Notify Patients of changes to Appointments

--- src/main/java/io/medrem/controllers/AppointmentController.java
+++ src/main/java/io/medrem/controllers/AppointmentController.java
@@ -1,16 +1,14 @@
 package io.medrem.controllers;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDate;
+
+
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
+
 import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.jobrunr.scheduling.JobScheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,6 +34,7 @@ import io.medrem.repository.AppointmentRepository;
 import io.medrem.repository.DoctorRepository;
 import io.medrem.repository.PatientRepository;
 import io.medrem.repository.UserRepository;
+import io.medrem.security.services.MedremMailSender;
 import io.medrem.security.services.UserDetailsImpl;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -55,12 +54,19 @@ public class AppointmentController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    MedremMailSender medremMailSender;
+
+    @Autowired
+    private JobScheduler jobScheduler;
+
     String regex = "(\\d{4})-(\\d{2})-(\\d{2})[T](\\d{2}):(\\d{2}):(\\d{2})";
 
-    
-
+    // 25.04.22 - Maybe i implement a generic create appointment at /appointment/new
+    // that takes patient_id,doctor_id? discuss merits
+    // with team morrow. But i kind of prefer this curent pattern.
     @PostMapping("/patients/{patientId}/appointment")
-    @PreAuthorize("hasRole('USER') or hasRole('RECEPTIONIST') or hasRole('ADMIN')")
+    @PreAuthorize("hasRole('USER') or hasRole('RECEPTIONIST') or hasRole('ADMIN') or hasRole('PHYSICIAN')")
     public ResponseEntity<?> createAppointment(@Valid @RequestBody AppointmentRequest appointmentRequest,
             @PathVariable("patientId") long patientId) {
         Patient patient = patientRepository.findById(patientId).orElse(null);
@@ -103,12 +109,18 @@ public class AppointmentController {
 
         appointmentRepository.save(appointment);
 
+        // Send Appointment Confirmation Email in the Background
+        jobScheduler.enqueue(() -> medremMailSender.sendAppointmentNotification(
+                String.valueOf(patient.getUser().getEmail()),
+                String.valueOf(doctor.getFirstname() + " " + doctor.getLastname()),
+                String.valueOf(appointmentSchedule), String.valueOf(patient.getFirstname())));
+
         return ResponseEntity.ok(new MessageResponse("Appointment registered successfully!"));
 
     }
 
     @PutMapping("/patients/{patientId}/appointments/{appointmentId}/edit")
-    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasRole('USER') or hasRole('RECEPTIONIST') or hasRole('ADMIN') or hasRole('PHYSICIAN')")
     public ResponseEntity<?> editAppointment(@PathVariable("patientId") long patientId,
             @PathVariable("appointmentId") long appointmentId,
             @Valid @RequestBody AppointmentRequest appointmentRequest) {
@@ -116,22 +128,28 @@ public class AppointmentController {
         Appointment appointment = appointmentRepository.findById(appointmentId).orElse(null);
         Doctor doctor = doctorRepository.findById(appointmentRequest.getDoctor().getId()).orElse(null);
 
-
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
+        Optional<User> optUser = userRepository.findById(userDetails.getId());
+        User user = optUser.get();
+     
         if (patient == null) {
             return ResponseEntity
                     .badRequest()
                     .body(new MessageResponse("Error: Patient with ID: " + patientId + " does not exist."));
         }
-
-        if (patient.getUser().getId() != patientId) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Error: You can only edit Your Own Details."));
+        
+        if (String.valueOf(user.getRoles().iterator().next().getName()) == "ROLE_USER") {
+            if (patient.getUser().getId() != user.getId()) {
+                return ResponseEntity
+                        .badRequest()
+                        .body(new MessageResponse("Error: You can only edit Your Own Details."));
+            }
         }
         if (appointment == null) {
             return ResponseEntity
                     .badRequest()
-                    .body(new MessageResponse("Error: Schedule with ID: " + appointmentId + " does not exist."));
+                    .body(new MessageResponse("Error: Appointment with ID: " + appointmentId + " does not exist."));
         }
 
         LocalDateTime appointmentSchedule;
@@ -150,36 +168,51 @@ public class AppointmentController {
         appointment.setLabel(appointmentRequest.getLabel());
 
         appointmentRepository.save(appointment);
+
+        // Send Appointment Confirmation Email in the Background
+        jobScheduler.enqueue(() -> medremMailSender.sendAppointmentModifiedNotification(
+                String.valueOf(patient.getUser().getEmail()),
+                String.valueOf(doctor.getFirstname() + " " + doctor.getLastname()),
+                String.valueOf(appointmentSchedule), String.valueOf(patient.getFirstname())));
         return ResponseEntity.ok(new MessageResponse("Appointment updated successfully!"));
     }
 
     @DeleteMapping("/patients/{patientId}/appointments/{appointmentId}")
-    @PreAuthorize("hasRole('USER') or hasRole('RECEPTIONIST') or hasRole('PHYSICIAN') or hasRole('ADMIN')")
+    @PreAuthorize("hasRole('USER') or hasRole('RECEPTIONIST') or hasRole('ADMIN')")
     public ResponseEntity<?> deleteAppointment(@PathVariable("patientId") long patientId,
             @PathVariable("appointmentId") long appointmentId) {
         Patient patient = patientRepository.findById(patientId).orElse(null);
         Appointment appointment = appointmentRepository.findById(appointmentId).orElse(null);
-
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-    UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
-    Optional<User> optUser = userRepository.findById(userDetails.getId());
-    User user = optUser.get();
+        UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
+        Optional<User> optUser = userRepository.findById(userDetails.getId());
+        User user = optUser.get();
 
         if (patient == null) {
             return ResponseEntity
                     .badRequest()
                     .body(new MessageResponse("Error: Patient with ID: " + patient + " does not exist."));
         }
-        if (user.getId() != patientId) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Error: You can only Delete Your Own Details."));
+        // So Only Receptionist, Doctors should be able to delete every appointment
+        if (String.valueOf(user.getRoles().iterator().next().getName()) == "ROLE_USER") {
+            if (patient.getUser().getId() != user.getId()) {
+                return ResponseEntity
+                        .badRequest()
+                        .body(new MessageResponse("Error: You can only Delete Your Own Details."));
+            }
         }
         if (appointment == null) {
             return ResponseEntity
                     .badRequest()
                     .body(new MessageResponse("Error: Appointment with ID: " + appointmentId + " does not exist."));
         }
+        
+        Doctor doctor = doctorRepository.findById(appointment.getDoctor().getId()).orElse(null);
+        jobScheduler.enqueue(() -> medremMailSender.sendAppointmentDeletedNotification(
+                String.valueOf(patient.getUser().getEmail()),
+                String.valueOf(doctor.getFirstname() + " " + doctor.getLastname()),
+                String.valueOf(appointment.getAppointmentTimestamp()), String.valueOf(patient.getFirstname())));
+
         appointmentRepository.deleteById(appointmentId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
